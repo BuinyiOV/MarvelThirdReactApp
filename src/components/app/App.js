ebe4035d126eb07373240d43eb37789268f365cc
Buinyi <buinyiow@gmail.com>
2023-08-18 11:43:38 +0200

    404 test deploy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -23,10 +23,9 @@ const App = () => {
 					<Suspense fallback={<Spinner/>}>
 						<Routes>
 							<Route path="/" element={<MainPage/>}/>
-							<Route path="https://buinyiov.github.io/MarvelThirdReactApp/" element={<MainPage/>}/>
-							<Route path="https://buinyiov.github.io/MarvelThirdReactApp" element={<MainPage/>}/>
-							<Route path="https://buinyiov.github.io" element={<MainPage/>}/>
-							<Route path="buinyiov.github.io/MarvelThirdReactApp/" element={<MainPage/>}/>
+							<Route path="/MarvelThirdReactApp/" element={<MainPage/>}/>
+							<Route path="MarvelThirdReactApp" element={<MainPage/>}/>
+							<Route path="MarvelThirdReactApp/" element={<MainPage/>}/>
 							<Route path="/comics" element={<ComicsPage/>}/>
 							<Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>
 							<Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
