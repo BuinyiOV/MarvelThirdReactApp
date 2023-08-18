5ae6e52ecfa2ccfa27fd1d28cb555c847a0f0393
Buinyi <buinyiow@gmail.com>
2023-08-18 12:48:13 +0200

    HashRouter replaced by BrowserRouter pade 404 replaced vby MainPage to avoid error on gh-pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 import { lazy, Suspense } from 'react';
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
 
-const Page404 = lazy(() => import('../pages/404'));
+// const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
 const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
@@ -16,7 +16,7 @@ const SinglePage = lazy(() => import('../pages/SinglePage'));
 const App = () => {
 
 	return (
-		<Router basename="/">
+		<Router>
 			<div className="app">
 				<AppHeader/>
 				<main>
@@ -26,7 +26,8 @@ const App = () => {
 							<Route path="/comics" element={<ComicsPage/>}/>
 							<Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic'/>}/>
 							<Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
-							<Route path="*" element={<Page404/>}/>
+							<Route path="*" element={<MainPage/>}/>
+							{/* <Route path="*" element={<Page404/>}/> */}
 						</Routes>
 					</Suspense>
 				</main>
